2ad0e1151b3047003aff04326272fc5bffc6f9a4
Kiera <kwright3@hotmail.co.uk>
2023-04-20 19:01:48 +0100

    Added ejs templating

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ let mysql = require('mysql');
 
 let express = require('express');
 let app = express();
-const PORT = 3000;
 
-const path = require("path");
 app.use(express.json());
 app.use(express.static("static"));
+app.set('view engine', 'ejs')
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -17,11 +16,24 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+// Creates a route that renders static/index.html
 app.get('/', function (req, res) {
     res.render("index.html")
 });
 
+// Creates a route that renders views/vinyls.ejs
 app.get('/vinyls', function (req, res) {
+    connection.query(`SELECT * FROM vinyl`, function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+        res.render("vinyls.ejs", { vinyls: results } )
+    });
+});
+
+const router = express.Router()
+router.use(express.json());
+
+router.get('/vinyls', function (req, res) {
     connection.query(`SELECT * FROM vinyl`, function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results);
@@ -29,7 +41,7 @@ app.get('/vinyls', function (req, res) {
     });
 });
 
-app.get('/vinyl-collections', function (req, res) {
+router.get('/vinyl-collections', function (req, res) {
     connection.query(`SELECT * FROM vinyl_collection`, function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results);
@@ -37,12 +49,15 @@ app.get('/vinyl-collections', function (req, res) {
     });
 });
 
-app.get('/vinyl-collection/:id', function (req, res) {
+// Uses route params to get vinyl collection id so it can use it in the SQL query
+router.get('/vinyl-collection/:id', function (req, res) {
     connection.query(`SELECT * FROM vinyl_collection WHERE vinyl_collection_id = '${req.params.id}'`, function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results);
         res.send(results)
     });
 });
+
+app.use('/api', router);
 
 app.listen(3000);
