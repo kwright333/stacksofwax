e33a7c544a19f35f96bc358663c60b4fb64d6d3b
Kiera <kwright3@hotmail.co.uk>
2023-04-22 20:05:26 +0100

    Adding modal card for vinyl details

--- static/index.js
+++ static/index.js
@@ -1,10 +1,17 @@
-addEventListener("load", (event) => {
-    const myRequest = new Request("http://localhost:3000/data");
+// addEventListener("load", (event) => {
+//     const myRequest = new Request("http://localhost:3000/data");
     
-    fetch(myRequest).then(response => {
-        console.log(response);
-        return response.json()
-    }).then(json => {
-        document.getElementById("test").innerText = JSON.stringify(json);
-    });
-})
+//     fetch(myRequest).then(response => {
+//         console.log(response);
+//         return response.json()
+//     }).then(json => {
+//         document.getElementById("test").innerText = JSON.stringify(json);
+//     });
+// });
+
+function showAlbumModal() {
+    const options = {
+
+    }
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), options)
+}
